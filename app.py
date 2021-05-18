c75d95d501edc7f752c03f28443b3d8035fa25e1
NikunjGoenka <Nikunj.Goenka#dal.ca>
2021-05-18 12:49:52 -0300

    new code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os 
 
 app = Flask(__name__)
 
@@ -9,4 +10,4 @@ def hello():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(port = port)
+	app.run(host="0.0.0.0",port = port)
